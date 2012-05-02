b63b90833cc24704a4ab9fe5d2b4b1ebada25c29
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-05-02 08:25:19 +0200

    added helper class for dumping html mail

---
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/MailDump.java
@@ -0,0 +1,44 @@
+
+package biz.vnc.zimbra.util;
+
+import com.zimbra.cs.zclient.ZMessage;
+import com.zimbra.cs.zclient.ZMessage.ZMimePart;
+
+public class MailDump
+{
+	/* dump a zimbra mail as HTML into a string buffer */
+	public static boolean dumpBodyHTML(ZMessage msg, StringBuffer sb)
+	{
+		if (msg == null)
+			return false;
+
+		ZMimePart mp = msg.getMimeStructure();
+		return dumpBodyHTML(mp, sb);
+	}
+
+	/* dump a zimbra mail part as HTML into a string buffer */
+	public static boolean dumpBodyHTML(ZMimePart mp, StringBuffer sb)
+	{
+		if (mp == null)
+			return false;
+
+		if (mp.isBody())
+		{
+			String ct = mp.getContentType();
+			if ((ct == null) || ct.equals("text/plain") || ct.equals(""))
+				sb.append("<pre>"+mp.getContent()+"</pre>");
+			else
+				sb.append(mp.getContent());
+			return true;
+		}
+		else
+		{
+			for (ZMimePart child : mp.getChildren())
+			{
+				if (dumpBodyHTML(child,sb))
+					return true;
+			}
+		}
+		return false;
+	}
+}
