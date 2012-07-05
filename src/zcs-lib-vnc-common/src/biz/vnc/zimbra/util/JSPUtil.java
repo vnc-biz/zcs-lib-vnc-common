fd275d55df3fb4bc740be9d35398d829b8303612
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-07-06 00:11:40 +0200

    helpers for retrieving current account

--- src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/JSPUtil.java
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/JSPUtil.java
@@ -5,6 +5,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Cookie;
 
 import com.zimbra.cs.zclient.ZMailbox;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.AuthToken;
+import com.zimbra.cs.account.AuthTokenException;
+import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.soap.SoapProvisioning;
 import com.zimbra.cs.account.soap.SoapProvisioning.Options;
 import com.zimbra.common.auth.ZAuthToken;
@@ -35,6 +39,24 @@ public class JSPUtil
 			}
 		}
 		return "";
+	}
+
+	public static AuthToken getAuthTokenObj(HttpServletRequest r)
+		throws AuthTokenException
+	{
+		return AuthToken.getAuthToken(getAuthToken(r));
+	}
+
+	public static String getCurrentAccountID(HttpServletRequest r)
+		throws AuthTokenException
+	{
+		return getAuthTokenObj(r).getAccountId();
+	}
+
+	public static Account getCurrentAccount(HttpServletRequest r)
+		throws AuthTokenException, ServiceException
+	{
+		return Provisioning.getInstance().get(Provisioning.AccountBy.id, getCurrentAccountID(r));
 	}
 
 	/* Reading email body via soap requests. */
