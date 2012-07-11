192425a76dc3d982efd9460316d94de01844f2df
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-07-11 09:38:39 +0200

    added ZLog - logging helper class

---
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/ZLog.java
@@ -0,0 +1,29 @@
+package biz.vnc.zimbra.util;
+
+public class ZLog
+{
+	public static void err(String facility, String message)
+	{
+		System.err.println("ERROR ["+facility+"] "+message);
+	}
+
+	public static void err(String facility, String message, Exception e)
+	{
+		System.err.println("ERROR ["+facility+"] "+message+" "+e.toString());
+	}
+
+	public static void warn(String facility, String message)
+	{
+		System.err.println("WARNING ["+facility+"] "+message);
+	}
+
+	public static void info(String facility, String message)
+	{
+		System.err.println("INFO ["+facility+"] "+message);
+	}
+
+	public static void debug(String facility, String message)
+	{
+		System.err.println("DEBUG ["+facility+"] "+message);
+	}
+}
