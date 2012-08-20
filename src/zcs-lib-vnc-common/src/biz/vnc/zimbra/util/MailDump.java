d4f79eef5c9e99fa29c9c479a105fe8fea9bf1aa
Bhavin Patel <bhavin.patel@vnc.biz>
2012-08-20 20:28:06 +0530

    MailDump: helpers for retrieving addresses from mails

--- src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/MailDump.java
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/MailDump.java
@@ -1,6 +1,8 @@
-
 package biz.vnc.zimbra.util;
 
+import java.util.Vector;
+import com.zimbra.common.util.ByteUtil;
+import com.zimbra.cs.zclient.ZEmailAddress;
 import com.zimbra.cs.zclient.ZMessage;
 import com.zimbra.cs.zclient.ZMessage.ZMimePart;
 
@@ -33,5 +35,41 @@ public class MailDump {
 			}
 		}
 		return false;
+	}
+
+	/* Get From address */
+	public static Vector<String> getFrom(ZMessage msg) {
+		Vector<String> from = new Vector<String>();
+
+		for(ZEmailAddress email : msg.getEmailAddresses()) {
+			if(ZEmailAddress.EMAIL_TYPE_FROM.equals(email.getType())) {
+				from.add(email.getAddress());
+			}
+		}
+		return from;
+	}
+
+	/* Get To address */
+	public static Vector<String> getTo(ZMessage msg) {
+		Vector<String> to = new Vector<String>();
+
+		for(ZEmailAddress email : msg.getEmailAddresses()) {
+			if(ZEmailAddress.EMAIL_TYPE_TO.equals(email.getType())) {
+				to.add(email.getAddress());
+			}
+		}
+		return to;
+	}
+
+	/* Get CC address */
+	public static Vector<String> getCC(ZMessage msg) {
+		Vector<String> cc = new Vector<String>();
+
+		for(ZEmailAddress email : msg.getEmailAddresses()) {
+			if(ZEmailAddress.EMAIL_TYPE_CC.equals(email.getType())) {
+				cc.add(email.getAddress());
+			}
+		}
+		return cc;
 	}
 }
