c40e5769f9981e9da359e393cf338f15420458bd
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-09-28 12:54:03 +0200

    added MiniProvisioning helper class

---
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/MiniProvisioning.java
@@ -0,0 +1,45 @@
+package biz.vnc.zimbra.util;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.AuthTokenException;
+import com.zimbra.cs.account.DistributionList;
+import com.zimbra.cs.account.soap.SoapProvisioning;
+import com.zimbra.cs.account.soap.SoapProvisioning.Options;
+import com.zimbra.cs.account.Provisioning.AccountBy;
+import com.zimbra.cs.account.Provisioning.DistributionListBy;
+import com.zimbra.cs.account.ZimbraAuthToken;
+
+public class MiniProvisioning {
+	private static SoapProvisioning prov = null;
+
+	public static SoapProvisioning getProv()
+	throws ServiceException {
+		if (prov == null) {
+			Options options = new Options();
+			options.setLocalConfigAuth(true);
+			prov = new SoapProvisioning(options);
+		}
+		return prov;
+	}
+
+	public static Account getAccountByName(String username)
+	throws ServiceException {
+		return getProv().get(AccountBy.name,username);
+	}
+
+	public static String getEncodedAuthTokenByUsername(String username)
+	throws ServiceException, AuthTokenException {
+		return new ZimbraAuthToken(getAccountByName(username)).getEncoded();
+	}
+
+	public String[] getMembers(String dlName)
+	throws ServiceException {
+		DistributionList dlist=getProv().get(DistributionListBy.name,dlName);
+
+		if(dlist==null)
+			return null;
+
+		return dlist.getAllMembers();
+	}
+}
