d8bb328e26813e5f75d4ffa3db9ab7934c874dd4
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-10-02 18:58:06 +0200

    StrUtil: added bin2hex() and md5_hash_hex() helper functions

--- src/zcs-lib-vnc-common/src/biz/vnc/util/StrUtil.java
+++ src/zcs-lib-vnc-common/src/biz/vnc/util/StrUtil.java
@@ -2,6 +2,8 @@
 package biz.vnc.util;
 
 import java.lang.StringBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -20,5 +22,32 @@ public class StrUtil {
 
 	public static boolean isBlank(String s) {
 		return ((s==null) || (s.equals("")));
+	}
+
+	public static String bin2hex(byte[] b)
+	{
+		if (b==null)
+			return "";
+
+		StringBuffer hexString = new StringBuffer();
+
+		for (int i=0; i<b.length; i++)
+			hexString.append(Integer.toHexString(0xFF & b[i]));
+
+		return hexString.toString();
+	}
+
+	public static String md5_hash_hex(String s)
+	{
+		try
+		{
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(s.getBytes());
+			return bin2hex(md.digest());
+		}
+		catch (java.security.NoSuchAlgorithmException e)
+		{
+			return "";
+		}
 	}
 }
