c3115c1491f99a82d6e87b4a5c03874d36ff68c3
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-11-07 08:09:36 +0100

    added StreamUtil class for loading an input stream into byte array

---
+++ src/zcs-lib-vnc-common/src/biz/vnc/util/StreamUtil.java
@@ -0,0 +1,23 @@
+
+package biz.vnc.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class StreamUtil {
+
+	public static byte[] readBytes(InputStream input)
+	throws IOException {
+		byte buffer[] = new byte[1024];
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+		int count = input.read(buffer);
+		while (0 < count) {
+			output.write(buffer, 0, count);
+			count = input.read(buffer);
+		}
+		input.close();
+		return output.toByteArray();
+	}
+}
