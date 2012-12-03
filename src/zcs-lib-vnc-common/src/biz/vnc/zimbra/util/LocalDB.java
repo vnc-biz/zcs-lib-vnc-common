68780994841d43f74edc22010eab5c702ed276e7
Hiren Sanghvi <hiren.sanghvi@vnc.biz>
2012-12-03 12:14:37 +0530

    [3914] - Added ?zeroDateTimeBehavior=convertToNull&autoReconnect=true to solve date time and database idle issue required for VNC CRM for Zimbra.

--- src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/LocalDB.java
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/LocalDB.java
@@ -10,7 +10,7 @@ public class LocalDB {
 	public static Connection connect(String dbname)
 	throws SQLException, ClassNotFoundException {
 		LocalConfig cf = LocalConfig.get();
-		String dbpath = "jdbc:mysql://"+cf.db_host+":"+cf.db_port+"/"+((dbname==null) ? cf.db_name : dbname);
+		String dbpath = "jdbc:mysql://"+cf.db_host+":"+cf.db_port+"/"+((dbname==null) ? cf.db_name : dbname)+"?zeroDateTimeBehavior=convertToNull&autoReconnect=true";
 		Class.forName(dbclass);
 		return DriverManager.getConnection(dbpath, cf.db_user, cf.db_password);
 	}
