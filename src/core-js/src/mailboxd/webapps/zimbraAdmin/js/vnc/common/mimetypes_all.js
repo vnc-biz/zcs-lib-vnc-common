a9ae9333074b4861392e2c28fe009f786bbf69e0
Hiren Sanghvi <hiren.sanghvi@vnc.biz>
2012-12-27 13:01:30 +0530

    [5895] - Added zimbraAdmin path for loading external libraries for admin side.

---
+++ src/core-js/src/mailboxd/webapps/zimbraAdmin/js/vnc/common/mimetypes_all.js
@@ -0,0 +1,57 @@
+
+if (AjxPackage.define("vnc.common.mimetypes")) {
+
+    if (vnc == undefined) {
+        var vnc = {};
+    }
+
+    if (vnc.common == undefined) {
+        vnc.common = {}; 
+    }
+
+    if (vnc.common.mimetypes == undefined) {
+        vnc.common.mimetypes = {}; 
+    }
+
+    if (vnc.common.mimetypes.list == undefined) {
+
+vnc.common.mimetypes.list = [
+"text/javascript",
+"application/x-javascript",
+"application/vnd.oasis.opendocument.text",
+"application/vnd.oasis.opendocument.text-template",
+"application/vnd.oasis.opendocument.graphics",
+"application/vnd.oasis.opendocument.graphics-template",
+"application/vnd.oasis.opendocument.presentation",
+"application/vnd.oasis.opendocument.presentation-template",
+"application/vnd.oasis.opendocument.spreadsheet",
+"application/vnd.oasis.opendocument.spreadsheet-template",
+"application/vnd.oasis.opendocument.chart",
+"application/vnd.oasis.opendocument.chart-template",
+"application/vnd.oasis.opendocument.image",
+"application/vnd.oasis.opendocument.image-template",
+"application/vnd.oasis.opendocument.formula",
+"application/vnd.oasis.opendocument.formula-template",
+"application/vnd.oasis.opendocument.text-master",
+"application/vnd.oasis.opendocument.text-web",
+"application/vnd.oasis.opendocument.database",
+"application/vnd.sun.xml.writer",
+"application/vnd.sun.xml.calc",
+"application/vnd.sun.xml.draw",
+"application/vnd.sun.xml.impress",
+"application/vnd.stardivision.draw",
+"application/vnd.stardivision.calc",
+"application/vnd.stardivision.impress",
+"application/vnd.stardivision.impress-packed",
+"application/vnd.stardivision.chart",
+"application/vnd.stardivision.writer",
+"application/vnd.stardivision.writer-global",
+"application/vnd.stardivision.math",
+"application/wordperfect",
+"audio/x-mpeg",
+"application/acp",
+"image/bmp"
+];
+
+    }
+}
