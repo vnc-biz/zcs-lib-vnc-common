b58df0c20a99c05a5b125d1c0be1d8dffa4541ec
Bhavin Patel <bhavin.patel@vnc.biz>
2013-03-14 12:16:39 +0530

    [4068] Connection pooling using dbcp

--- src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/LocalDB.java
+++ src/zcs-lib-vnc-common/src/biz/vnc/zimbra/util/LocalDB.java
@@ -1,17 +1,31 @@
 package biz.vnc.zimbra.util;
 
+import biz.vnc.zimbra.util.ZLog;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDriver;
+import org.apache.commons.pool.impl.GenericObjectPool;
 
 public class LocalDB {
-	private static final String dbclass = "com.mysql.jdbc.Driver";
-
 	public static Connection connect(String dbname)
 	throws SQLException, ClassNotFoundException {
 		LocalConfig cf = LocalConfig.get();
 		String dbpath = "jdbc:mysql://"+cf.db_host+":"+cf.db_port+"/"+((dbname==null) ? cf.db_name : dbname)+"?zeroDateTimeBehavior=convertToNull&autoReconnect=true&characterEncoding=UTF-8";
-		Class.forName(dbclass);
-		return DriverManager.getConnection(dbpath, cf.db_user, cf.db_password);
+
+		GenericObjectPool pool = new GenericObjectPool(null);
+		// use the connection factory which will wraped by
+		// the PoolableConnectionFactory
+		DriverManagerConnectionFactory dm =  new DriverManagerConnectionFactory(dbpath,cf.db_user,cf.db_password);
+
+		PoolableConnectionFactory pcf =  new PoolableConnectionFactory(dm, pool, null, null, false, true);
+		// register our pool and give it a name
+		new PoolingDriver().registerPool("myPool", pool);
+		// get a connection and test it
+		Connection conn = DriverManager.getConnection("jdbc:apache:commons:dbcp:myPool");
+		// now we can use this pool the way we want.
+		return conn;
 	}
 }
